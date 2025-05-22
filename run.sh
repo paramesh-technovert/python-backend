c7ec4e7173647000bcb87ce6bf18caff23638d78
Shilpa Eppa <seppa@mtm-inc.net>
2025-05-22 13:42:15 +0530

    Made some changes in run.sh for automating venv creation and dependies installation

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Create virtual environment if it doesn't exist
+if [ ! -d ".venv" ]; then
+  echo "Creating virtual environment..."
+  python -m venv .venv
+fi
+
+# Automatically install requirements recommended only for local use 
+pip install -r requirements.txt 
+
 # Activate virtual environment (Windows path)
 source .venv/Scripts/activate
 
 # Run FastAPI using Python module to avoid permission issues
-python -m uvicorn app.main:app --reload --host 127.0.0.1 --port 8000
+python -m uvicorn app.main:app --reload --host 127.0.0.1 --port 8000 
